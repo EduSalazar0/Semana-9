a61dff30b2fb64d3f33256d225de92a9b8fe2298
EduSalazar0 <edutebansalazar95@gmail.com>
2023-05-22 16:07:10 -0500

    Definicion de funciones matrices numeros randomicos

--- MatrizTranspuesta.c
+++ MatrizTranspuesta.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define filas 50
-#define columnas 50
+#define filas 1000
+#define columnas 1000
 int main(){
 int i,j,f,c; //Defino variablesindexadoras y variables para tomar el valor de filas,columnas y matrices
 int A[filas][columnas]; // Defino una matriz de n matrices, n filas y n columnas
@@ -14,17 +14,25 @@ int A[filas][columnas]; // Defino una matriz de n matrices, n filas y n columnas
     //Funcion para formacion de matrices con numeros randomicos
     for(i=0;i<f;i++){
         for(j=0;j<c;j++){
-            A[f][c] = rand()%100;
+            A[f][c] = rand( ) % 100;
         }
     }
     system("cls");
-    //Funcion para imprimir las matrices deseadas
+    //Funcion para imprimir las matrices con numeros randomicos
     printf("Impresion de las matrices \n");
     for(i=0;i<f;i++){
         for(j=0;j<c;j++){
                 printf(" (%d) ",A[i][j]);
             }
+            printf("\n");
+        }
+    //Funcion para imprimir la Matriz transpuesta
+    printf("La matriz transpuesta es \n");
+    for(i=0;i<f;i++){
+        for(j=0;j<c;j++){
+            printf(" (%d) ",A[j][i]);
         }
         printf("\n");
+    }
     return 0;
 }
