c86ae1c146cb582acf99eea91ace0e6533e4b665
EduSalazar0 <edutebansalazar95@gmail.com>
2023-05-22 16:33:13 -0500

    Codigo Final pero con falla en la funcion randomica, REVISAR

--- MatrizTranspuesta.c
+++ MatrizTranspuesta.c
@@ -1,35 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define filas 1000
-#define columnas 1000
+#include<time.h>
+#define filas 50
+#define columnas 50
 int main(){
-int i,j,f,c; //Defino variablesindexadoras y variables para tomar el valor de filas,columnas y matrices
+srand(time(NULL));
+int f,c; //Defino variablesindexadoras y variables para tomar el valor de filas,columnas y matrices
 int A[filas][columnas]; // Defino una matriz de n matrices, n filas y n columnas
 //Definicion y toma de valores por parte del usurio
     printf("De cuantas filas va a querer su matriz? ");
     scanf("%d",&f);
     printf("De cuantas columnas va a querer su matriz? ");
     scanf("%d",&c);
-    system("cls");
-    //Funcion para formacion de matrices con numeros randomicos
-    for(i=0;i<f;i++){
-        for(j=0;j<c;j++){
-            A[f][c] = rand( ) % 100;
+    for(int i=0;i<f;i++){
+        for(int j=0;j<c;j++){
+            A[f][c] = 0;
         }
     }
-    system("cls");
-    //Funcion para imprimir las matrices con numeros randomicos
-    printf("Impresion de las matrices \n");
-    for(i=0;i<f;i++){
-        for(j=0;j<c;j++){
+    //Funcion para formacion de matrices con numeros randomicos
+    for(int i=0;i<f;i++){
+        for(int j=0;j<c;j++){
+            A[f][c] = rand()%100;
+        }
+    }
+    //Funcion para imprimir la matrices con numeros randomicos
+    printf("Impresion de la matrices \n");
+    for(int i=0;i<f;i++){
+        for(int j=0;j<c;j++){
                 printf(" (%d) ",A[i][j]);
             }
             printf("\n");
         }
     //Funcion para imprimir la Matriz transpuesta
     printf("La matriz transpuesta es \n");
-    for(i=0;i<f;i++){
-        for(j=0;j<c;j++){
+    for(int i=0;i<f;i++){
+        for(int j=0;j<c;j++){
             printf(" (%d) ",A[j][i]);
         }
         printf("\n");
